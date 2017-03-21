25e71a1a229605087bae50bcd5bbffb1ffdf393d
Kids <Kids@deansmacbookair.telus>
2017-03-20 18:25:59 -0600

    - added fire works to LevelCompleteScene.swift

--- EarthDefender/LevelCompleteScene.swift
+++ EarthDefender/LevelCompleteScene.swift
@@ -13,6 +13,8 @@ class LevelCompleteScene: SKScene {
 
     let background = SKSpriteNode(imageNamed: "EarthDefenderLevelCompleteScene")
     
+
+    
     override func didMove(to view: SKView) {
        background.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
         background.zPosition = 1
@@ -20,13 +22,19 @@ class LevelCompleteScene: SKScene {
         background.yScale = 1.60
     
         addChild(background)
+        fireWork(position: CGPoint(x: 100, y: 200))
+        fireWork(position: CGPoint(x: 200, y: 400))
+        fireWork(position: CGPoint(x: 100, y: 600))
+        fireWork(position: CGPoint(x: 300, y: 500))
+
+
         
-        func fireWork(position: CGPoint) {
-            if let particles = SKEmitterNode(fileNamed: "FireWork.sks") {
-                particles.position = position
-                particles.zPosition = background.zPosition + 1
-                addChild(particles)
-            }
+    }
+    func fireWork(position: CGPoint) {
+        if let particles = SKEmitterNode(fileNamed: "FireWork.sks") {
+            particles.position = position
+            particles.zPosition = background.zPosition + 1
+            addChild(particles)
         }
     }
 }
---
+++ EarthDefender/player.swift
@@ -0,0 +1,10 @@
+//
+//  player.swift
+//  EarthDefender
+//
+//  Created by Kids on 2017-03-20.
+//  Copyright © 2017 Sam. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
