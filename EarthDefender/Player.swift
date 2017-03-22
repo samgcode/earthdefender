8a64d3926ca1bfbf8e8cd708dd43bb6a004a1660
Kids <Kids@DeansMacbookAir.telus>
2017-03-21 18:31:08 -0600

    - rename player.swift

---
+++ EarthDefender/Player.swift
@@ -0,0 +1,35 @@
+//
+//  player.swift
+//  EarthDefender
+//
+//  Created by Kids on 2017-03-20.
+//  Copyright © 2017 Sam. All rights reserved.
+//
+
+import Foundation
+
+class Player {
+    
+    var lives: Int
+    var totalMonstersKilled: Int
+    var monstersLeftForLevel: Int
+    
+    init(lives: Int) {
+        self.lives = lives
+        self.totalMonstersKilled = 0
+        self.monstersLeftForLevel = 25
+    }
+    
+    func incrumentMonsterCount() {
+        totalMonstersKilled += 1
+    }
+    
+    func decrementLives() {
+        lives -= 1
+    }
+    
+    func decrementMonstersLeft() {
+        monstersLeftForLevel -= 1
+    }
+
+}
