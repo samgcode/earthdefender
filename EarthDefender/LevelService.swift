04f6501105d62401e0c60f5828040dc2a0421109
Sam <samgcode@gmail.com>
2017-07-09 19:38:38 -0600

    changed if to switch in level service removed monster amount func

--- EarthDefender/LevelService.swift
+++ EarthDefender/LevelService.swift
@@ -12,14 +12,11 @@ import SpriteKit
 class LevelService {
     static let sharedInstance = LevelService()
     private(set) var currentLevel: Int
-    var monsterCount: Int
     var player: Player = Player.sharedInstance
     
     init() {
         self.currentLevel = 0
-        monsterCount = 0
     }
-    
     
     func loadTitleScene(size: CGSize) -> SKScene {
         return TitleScene(size: size)
@@ -28,52 +25,41 @@ class LevelService {
     func loadNextLevel(size: CGSize) -> SKScene {
         currentLevel += 1
         player.incrementLives()
+        var monsterType: MonsterType = .commet
+        var monsterCount = 25
+        var backgroundType: BackgroundType = .earth
         
-        if (currentLevel == 1 || currentLevel == 2 || currentLevel == 3 || currentLevel == 4) {
-            return GameScene.init(monster: .commet, size: size, numberOfMonsters: monsterAmount(), backgroundType: .earth)
-        } else if currentLevel == 5{
+        switch currentLevel {
+        case 1, 2, 3, 4:
+            monsterCount = currentLevel * 5 + 20
+        case 5:
             return Level2.init(monster: .commet, size: size, backgroundType: .earth, boss: .commetBoss)
-        } else if (currentLevel == 6 || currentLevel == 7 || currentLevel == 8 || currentLevel == 9) {
-            return GameScene.init(monster: .asteroid, size: size, numberOfMonsters: monsterAmount(), backgroundType: .earth)
-        } else if currentLevel == 10 {
+        case 6, 7, 8, 9:
+            monsterType = .asteroid
+            monsterCount = currentLevel * 5 - 5
+            backgroundType = .earth
+        case 10:
             return Level2.init(monster: .asteroid, size: size, backgroundType: .earth, boss: .asteroidBoss)
-        } else if (currentLevel == 11 || currentLevel == 12 || currentLevel == 13 || currentLevel == 14) {
-            return GameScene.init(monster: .spaceship, size: size, numberOfMonsters: monsterAmount(), backgroundType: .mars)
-        } else if currentLevel == 15 {
+        case 11, 12, 13, 14:
+            monsterType = .spaceship
+            monsterCount = currentLevel * 5 - 30
+            backgroundType = .mars
+        case 15:
             return Level2.init(monster: .spaceship, size: size, backgroundType: .mars, boss: .spaceshipBoss1)
-        } else if (currentLevel == 16 || currentLevel == 17 || currentLevel == 18 || currentLevel == 19) {
-            return GameScene.init(monster: .spaceship2, size: size, numberOfMonsters: monsterAmount(), backgroundType: .mars)
-        } else if currentLevel == 20 {
+        case 16, 17, 18, 19:
+            monsterType = .spaceship2
+            monsterCount = currentLevel * 5 - 55
+            backgroundType = .mars
+        case 20:
             return Level2.init(monster: .spaceship2, size: size, backgroundType: .mars, boss: .spaceshipBoss2)
-        } else {
-            return GameScene.init(monster: .commet, size: size, numberOfMonsters: monsterAmount(), backgroundType: .earth)
-      }
-   
+        default:
+            return GameScene.init(monster: monsterType, size: size, numberOfMonsters: monsterCount, backgroundType: backgroundType)
+        }
+        return GameScene.init(monster: monsterType, size: size, numberOfMonsters: monsterCount, backgroundType: backgroundType)
     }
     
     func loadLevelComplete(size: CGSize) -> SKScene {
         return LevelCompleteScene(size: size, level: currentLevel)
-    }
-    func monsterAmount() -> Int {
-        if (currentLevel == 1 || currentLevel == 2 || currentLevel == 3 || currentLevel == 4) {
-            monsterCount = currentLevel * 5
-            monsterCount += 20
-            return monsterCount
-        } else if (currentLevel == 6 || currentLevel == 7 || currentLevel == 8 || currentLevel == 9) {
-            monsterCount = currentLevel * 5
-            monsterCount -= 5
-            return monsterCount
-        } else if (currentLevel == 11 || currentLevel == 12 || currentLevel == 13 || currentLevel == 14){
-            monsterCount = currentLevel * 5
-            monsterCount -= 30
-            return monsterCount
-        } else if (currentLevel == 16 || currentLevel == 17 || currentLevel == 18 || currentLevel == 19) {
-            monsterCount = currentLevel * 5
-            monsterCount -= 55
-            return monsterCount
-        } else {
-            return 0
-        }
     }
     
     func resetCurrentLevel() {
