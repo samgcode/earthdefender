27fd0282b4f63ab47b5f6123c32c9c4bb6d12378
Sam <samgcode@gmail.com>
2017-07-11 09:06:14 -0600

    added level beat and monsters killed labels to game complete

--- EarthDefender/GameCompleteScene.swift
+++ EarthDefender/GameCompleteScene.swift
@@ -10,7 +10,11 @@ import Foundation
 import SpriteKit
 
 class GameCompleteScene: SKScene {
-    var background = SKSpriteNode(imageNamed: "EarthDefenderGameCompleteScene")
+    let player: Player = Player.sharedInstance
+    let levelService: LevelService = LevelService.sharedInstance
+    let background = SKSpriteNode(imageNamed: "EarthDefenderGameCompleteScene")
+    let monstersKilledLabel: SKLabelNode = SKLabelNode()
+    let levelsBeatLabel: SKLabelNode = SKLabelNode()
     
     override func didMove(to view: SKView) {
         background.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
@@ -18,6 +22,22 @@ class GameCompleteScene: SKScene {
         background.xScale = 0.90
         background.yScale = 1.20
         
+        monstersKilledLabel.position = CGPoint(x: 155, y: 200)
+        monstersKilledLabel.zPosition = 100
+        monstersKilledLabel.text = "you killed \(player.totalMonstersKilled) monsters"
+        monstersKilledLabel.fontSize = 25
+        monstersKilledLabel.fontColor = UIColor.cyan
+        monstersKilledLabel.fontName = "AmericanTypewriter"
+        
+        levelsBeatLabel.position = CGPoint(x: 155, y: 150)
+        levelsBeatLabel.zPosition = 100
+        levelsBeatLabel.text = "you beat \(levelService.currentLevel - 1) levels"
+        levelsBeatLabel.fontSize = 25
+        levelsBeatLabel.fontColor = UIColor.orange
+        levelsBeatLabel.fontName = "AmericanTypewriter"
+        
+        addChild(levelsBeatLabel)
+        addChild(monstersKilledLabel)
         addChild(background)
     }
     
